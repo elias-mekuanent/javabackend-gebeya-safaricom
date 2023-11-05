80128d7662c07d816f6c716a89b6723334b8c743
Kindie Nega <kindienega10@gmail.com>
2023-11-05 10:21:05 -0800

    an other try on system exceptions handler

---
+++ Lab1Solution/src/ChatExceptions/SystemExceptionHandler.java
@@ -0,0 +1,21 @@
+package ChatExceptions;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class SystemExceptionHandler implements Thread.UncaughtExceptionHandler {
+    private static final String LOG_FILE = "system.log";
+
+    @Override
+    public void uncaughtException(Thread t, Throwable e) {
+        try {
+            PrintWriter writer = new PrintWriter(new FileWriter(LOG_FILE, true));
+            writer.println("Exception in thread \"" + t.getName() + "\":");
+            e.printStackTrace(writer);
+            writer.close();
+        } catch (IOException ioe) {
+            System.err.println("Error writing exception to log file: " + ioe.getMessage());
+        }
+    }
+}
