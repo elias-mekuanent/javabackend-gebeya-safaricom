6e0c7fbc9d255a84489bb1c73435f80b3c1a26dc
Kindie Nega <kindienega10@gmail.com>
2023-11-05 10:13:47 -0800

    public chat exceptions trial

---
+++ Lab1Solution/src/ChatExceptions/PublicChatExceptions.java
@@ -0,0 +1,22 @@
+package ChatExceptions;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.List;
+
+public class PublicChatExceptions {
+
+        public void saveChatLog(String fileName, List<String> chatLog) {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(fileName))) {
+            for (String message : chatLog) {
+                writer.write(message);
+                writer.newLine();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    
+}
+
