d5975590ab23b85bec21e69671d3194957009d3b
Elias Mekuanent <121854563+elias-mekuanent@users.noreply.github.com>
2023-11-16 10:50:30 -0500

    Delete uneneccessary directory

--- CRUDRestApi/src/main/java/com/safaricom/gebeya/Repository/BookRepository.java
+++
@@ -1,9 +0,0 @@
-package com.safaricom.gebeya.Repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.safaricom.gebeya.Model.Book;
-
-public interface BookRepository extends JpaRepository<Book, Integer> {
-
-}
