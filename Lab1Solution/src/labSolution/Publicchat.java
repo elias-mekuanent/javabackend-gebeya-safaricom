b6dba76982779eca7686a55b89c7adb2d0857ed2
Elias Mekuanent <121854563+elias-mekuanent@users.noreply.github.com>
2023-11-16 15:41:28 -0500

    comments added on  Publicchat.java

--- Lab1Solution/src/labSolution/Publicchat.java
+++ Lab1Solution/src/labSolution/Publicchat.java
@@ -1,37 +1,40 @@
+// Package declaration
 package labSolution;
 
-//import java.io.*;
+// Import statements
 import java.util.*;
 
 import ChatExceptions.PublicChatExceptions;
 
+// Publicchat class definition
 public class Publicchat {
+    // Instance variables for public chat log and current user
     List<String> publicChatLog;
     String currentUser;
 
+    // Constructor to initialize public chat log and current user
     public Publicchat(List<String> publicChatLog, String currentUser) {
         this.publicChatLog = publicChatLog;
         this.currentUser = currentUser;
     }
 
+    // Method to send a public message
     public void sendPublicMessage(Scanner scanner) {
         System.out.println("Enter your message in the public chat:");
         String message = scanner.nextLine();
-        publicChatLog.add("you "+ "> " + message);
-        PCExceptios(ChatApp.PUBLIC_CHAT_LOG_FILE, publicChatLog);
+        publicChatLog.add("you " + "> " + message);
+        // Save public chat log with exceptions handling
+        PCExceptions(ChatApp.PUBLIC_CHAT_LOG_FILE, publicChatLog);
         System.out.println("Message sent to the public chat.");
     }
-    /*
- * using public chat writing exceptions 
- * Kindie Nega
- * back-end
- */
-    private void PCExceptios(String privateChatLogFile, List<String> privateChatLog2) {
-    
-        PublicChatExceptions PCExceptions = new PublicChatExceptions();
-          PCExceptions.PublicExceptions(privateChatLogFile, privateChatLog2);
-       }
 
+    // Method to handle exceptions while saving public chat log
+    private void PCExceptions(String publicChatLogFile, List<String> publicChatLog) {
+        PublicChatExceptions PCExceptions = new PublicChatExceptions();
+        PCExceptions.PublicExceptions(publicChatLogFile, publicChatLog);
+    }
+
+    // Method to display public chat messages
     public void showPublicChatMessages() {
         System.out.println("Public Chat Messages:");
         for (String message : publicChatLog) {
@@ -39,3 +42,4 @@ public class Publicchat {
         }
     }
 }
+/*commented by Elias Mekuanent*/
